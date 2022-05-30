d8a358bb1960ebb0b70f0fafd8b5576f451448dc
Bolaji5821 <oyedelebolaji84@gmail.com>
2022-05-30 16:53:37 -0700

    copying strings to variables

---
+++ 0x00-python-hello_world/7-edeges.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+word = "Holberton"
+word_first_3 = f"{word[0:3]}"
+word_last_two = f"{word[7:]}"
+middle_word = f"{word[1:-1]}"
+print(f"First 3 letters: {word_first_3}")
+print(f"Last 2 letters: {word_last_two}")
+print(f"Middle word: {middle_word}")
