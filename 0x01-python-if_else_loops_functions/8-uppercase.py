ccd4a40f0b5168b79ecd444616a5009910d4fdac
Bolaji5821 <oyedelebolaji84@gmail.com>
2022-06-01 08:28:10 -0700

    This function prints letters in uppercase

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -1,9 +1,10 @@
-#!/usr/bin/python3
+#!/usr/bin/python
 def uppercase(str):
     for c in str:
         if ord(c) >= ord('a') and ord(c) <= ord('z'):
             char = chr(ord(c) - 32)
-    else:
-        char = c
-        print("{:s}".format(char), end="")
-        print('')
+        else:
+            char = c
+            print("{:s}".format(char), end="")
+            print('')
+
