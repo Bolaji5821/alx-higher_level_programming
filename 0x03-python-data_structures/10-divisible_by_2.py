4cad1d1538235eb1ec6dd0c62dd46dea6b36b181
Bolaji5821 <oyedelebolaji84@gmail.com>
2022-06-06 19:19:19 -0700

    hot fix

--- 0x03-python-data_structures/10-divisible_by_2.py
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 def divisible_by_2(my_list=[]):
-    divi = []
+    new_list = []
     for i in range(len(my_list)):
         if my_list[i] % 2 == 0:
-            divi.append(True)
+            new_list.append(True)
         else:
-            divi.append(False)
-    return divi    
+            new_list.append(False)
+    return new_list
