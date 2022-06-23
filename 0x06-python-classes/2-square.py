5ae0787ae5f6d214995d14ff2029007317217228
Bolaji5821 <oyedelebolaji84@gmail.com>
2022-06-22 21:28:10 -0700

    Type err

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -1,14 +1,17 @@
 #!/usr/bin/python3
+""" Class to define a square"""
+
+
 class Square:
-    """Represents a square.
-    Private instance attribute: size.
-    Instantiation with optional size.
-    """
+    """ Create class instances """
 
     def __init__(self, size=0):
-        """Initializes the data."""
+        """Method to declare Square private instances
+        Args:
+                size(int): size of the square
+        """
         if not isinstance(size, int):
-            raise TypeError("size must be an integer")
-        elif size < 0:
-            raise ValueError("size must be >= 0")
+            raise TypeError('size must be an integer')
+        elif(size < 0):
+            raise ValueError('size must be >= 0')
         self.__size = size
