deb778a1e6d5b8842abbae4313b8a8b28b5a4ea5
Bolaji5821 <oyedelebolaji84@gmail.com>
2022-07-05 13:18:47 -0700

    Initial commit

---
+++ 0x0A-python-inheritance/0-lookup.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""Module 0-lookup
+This function returns all the attributes and method of an object
+"""
+
+
+def lookup(obj):
+    """Returns a list of attributes and methods.
+
+    Parameter: object or class to iterate
+    """
+
+    return obj.__dict__
---
+++ 0x0A-python-inheritance/README.md
@@ -0,0 +1,1 @@
+This directory contains source files and programs for alx python inheritance projects
