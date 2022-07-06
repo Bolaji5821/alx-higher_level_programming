97a20a6f03edb1273a1d30b8510768a9d84aeade
Bolaji5821 <oyedelebolaji84@gmail.com>
2022-07-05 17:09:08 -0700

    2-is same class

--- 0x0A-python-inheritance/2-is_same_class.py
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -8,7 +8,7 @@ the specified class ; otherwise False.
 def is_same_class(obj, a_class):
     """isintance method is used to compare the object"""
 
-    if isinstance(obj, a_class):
+    if type(obj) is a_class:
         return True
     else:
         return False
