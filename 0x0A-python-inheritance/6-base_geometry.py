c186837eee53b88d252ff7a595fb4e3013fb8a15
Bolaji5821 <oyedelebolaji84@gmail.com>
2022-07-05 17:50:23 -0700

    6 base geo

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""Module BaseGeometry:
+    a superclass
+"""
+
+
+class BaseGeometry:
+    """a superclass"""
+
+    def area(self):
+        """area method"""
+        raise Exception("area() is not implemented")
