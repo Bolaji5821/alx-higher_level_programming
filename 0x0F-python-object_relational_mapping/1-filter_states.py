3e1e30baae400ee54a11c558bfba72c90f12436c
Bolaji5821 <oyedelebolaji84@gmail.com>
2022-09-09 10:54:18 -0700

    List states with N

---
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""a script that lists all states with a name starting with
+N (upper N) from the database passed as argument"""
+if __name__ == '__main__':
+    import MySQLdb
+    import sys
+
+    db = MySQLdb.connect(host='localhost', port=3306, user=sys.argv[1],
+                         passwd=sys.argv[2], db=sys.argv[3])
+    cur = db.cursor()
+    cur.execute("SELECT * FROM states WHERE name LIKE 'N%'
+                ORDER BY states.id ASC")
+    rows = cur.fetchall()
+    for row in rows:
+        print(row)
