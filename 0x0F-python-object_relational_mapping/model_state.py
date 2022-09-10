9a7e639be56ac73db37ce7bb6aba01c9100a8f92
Bolaji5821 <oyedelebolaji84@gmail.com>
2022-09-09 20:06:22 -0700

    The state class model

--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -1,14 +1,14 @@
 #!/usr/python3
 """Contains the definition of state class"""
+
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy import Column, Integer, String
-
 
 Base = declarative_base()
 
 
 class State(Base):
-    """The state class, links to MySQL table states"""
+    """Class State"""
 
     __tablename__ = 'states'
     id = Column(Integer, autoincrement=True,
